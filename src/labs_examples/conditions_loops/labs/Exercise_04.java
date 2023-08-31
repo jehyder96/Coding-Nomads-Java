aa291c768713496ff9470f0b7763a7d141bc0da4
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-08-30 23:41:00 -0500

    "Condition and loops exercise 4 complete"

--- src/labs_examples/conditions_loops/labs/Exercise_04.java
+++ src/labs_examples/conditions_loops/labs/Exercise_04.java
@@ -6,7 +6,6 @@ package labs_examples.conditions_loops.labs;
  *      Using a "for-loop", print out all even numbers from 1-100.
  *
  */
-
 public class Exercise_04 {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
