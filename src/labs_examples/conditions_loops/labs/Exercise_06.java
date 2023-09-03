6d1b1da9f5a36f1cc4512ce77ab62bfed104e5a3
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-09-02 19:18:42 -0500

    "Condition and loops exercise 6 complete"

--- src/labs_examples/conditions_loops/labs/Exercise_06.java
+++ src/labs_examples/conditions_loops/labs/Exercise_06.java
@@ -8,4 +8,13 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_06 {
+    public static void main(String[] args) {
+        int x = 1; // initialize x to 1
+        int sum = 0; // initialize sum to 0
+        while(x <= 100){ // while x is less than or equal to 100
+            sum += x; // add x to sum
+            x++; // increment x
+        }
+        System.out.println("The sum from 1 to 100 is: " + sum);
+    }
 }
