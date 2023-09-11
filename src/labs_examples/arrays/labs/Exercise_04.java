0f353250fe0ab9827104948da63fe97f5d6ca60a
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-09-11 14:13:24 -0500

    "Arrays exercise 4 complete"

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -9,5 +9,15 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+        int[][] twoD = new int[3][3]; // creating the two-dimensional array
+        for (int i = 0; i < twoD.length; i++){ // creating the four loop
+            for (int j = 0; j < twoD[i].length; j++) { // creating the nested for loop
+                twoD[i][j] = i * j; // assigning the value of i * j to the array
+                System.out.print(twoD[i][j] + " - "); // printing the array
+            }
+            System.out.println(); // printing a new line
+        }
+    }
 
 }
