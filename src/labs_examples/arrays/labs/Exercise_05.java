06101dec76fb1d7fd3c3caf808659b58d2d3ab8c
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-09-11 15:09:37 -0500

    "Arrays exercise 5 complete"

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -9,5 +9,11 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7}; // creating the array
+        for (int i = array.length - 1; i >= 0; i-=2) { // creating the for loop to go through every other element in reverse order
+            System.out.println(array[i]); // printing the array
+        }
+    }
 
 }
