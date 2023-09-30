76884a9c30fe8d23a13c3dd9f1c52ed75ff4c9fc
Jordan Hyder <68782267+jehyder96@users.noreply.github.com>
2023-09-30 14:35:30 -0500

    "Exception handling labs 1 - 2 completed"

--- src/labs_examples/exception_handling/labs/Exercise_01.java
+++ src/labs_examples/exception_handling/labs/Exercise_01.java
@@ -7,3 +7,14 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+public class Exercise_01 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5}; // array of 5 elements
+
+        try {
+            System.out.println(nums[5]); // trying to access 6th element
+        } catch (ArrayIndexOutOfBoundsException exc) { // catching exception
+            System.out.println("Array index out of bounds."); // printing error message
+        }
+}
+}
--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -1,4 +1,6 @@
 package labs_examples.exception_handling.labs;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * Exception Handling Exercise 2:
@@ -7,4 +9,38 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+public class Exercise_02 {
+    public static void main(String[] args) {
+
+        // exception = 	an event that occurs during the execution of a program that,
+        //				disrupts the normal flow of instructions
+
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+            System.out.println("Enter a whole number to divide: ");
+            int x = scanner.nextInt();
+
+            System.out.println("Enter a whole number to divide by: ");
+            int y = scanner.nextInt();
+
+            int z = x/y;
+
+            System.out.println("result: " + z);
+        }
+        catch(ArithmeticException e) { // catches divide by zero
+            System.out.println("You can't divide by zero!");
+        }
+        catch(InputMismatchException e) { // catches non-integer input
+            System.out.println("PLEASE ENTER A NUMBER!!!");
+        }
+        catch(Exception e) { // catches all other exceptions
+            System.out.println("Something went wrong");
+        }
+        finally {
+            scanner.close();
+        }
+
+    }
+}
 
